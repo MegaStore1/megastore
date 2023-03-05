4c91a8b83bdaf256ddbacb085ec9a756ae67b9ad
Mohammad Naim Dahee <mohammad.dahee@mic-cust.com>
2023-03-05 22:10:40 +0100

    MS-3: Global Exception handler added to the app

--- MegaStore.API/Program.cs
+++ MegaStore.API/Program.cs
@@ -1,6 +1,9 @@
+using System.Net;
 using System.Text;
 using MegaStore.API.Data;
+using MegaStore.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
@@ -42,6 +45,25 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler(builder =>
+    {
+        builder.Run(async context =>
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var error = context.Features.Get<IExceptionHandlerFeature>();
+
+            if (error != null)
+            {
+                context.Response.AddApplicationError(error.Error.Message);
+                await context.Response.WriteAsync(error.Error.Message);
+            }
+        });
+    });
 }
 
 // app.UseHttpsRedirection();
